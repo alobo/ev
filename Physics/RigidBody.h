ebfbce12eb07672d1c8203e53ba3a20942eab961
Aaron Lobo <lobo.aaron1@gmail.com>
2016-02-14 18:20:57 -0800

    Initialize vectors to zero

--- Physics/RigidBody.h
+++ Physics/RigidBody.h
@@ -6,8 +6,8 @@ class RigidBody {
 public:
     float mass;
     float radius;
-    Eigen::Vector2f size;
-    Eigen::Vector2f position;
-    Eigen::Vector2f velocity;
-    Eigen::Vector2f acceleration;
+    Eigen::Vector2f size = Eigen::Vector2f::Zero();
+    Eigen::Vector2f position = Eigen::Vector2f::Zero();
+    Eigen::Vector2f velocity = Eigen::Vector2f::Zero();
+    Eigen::Vector2f acceleration = Eigen::Vector2f::Zero();
 };
